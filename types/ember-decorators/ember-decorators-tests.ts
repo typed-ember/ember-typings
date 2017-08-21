741fc967ca7b0e45dfaf44fabf45f466399d8dc7
Derek Wickern <dwickern@gmail.com>
2017-08-21 09:37:17 -0700

    start typings for ember-decorators

---
+++ types/ember-decorators/ember-decorators-tests.ts
@@ -0,0 +1,37 @@
+import Ember from 'ember';
+import computed from 'ember-decorators/object';
+import { alias, and } from 'ember-decorators/object/computed';
+
+/** Static assertion that `value` has type `T` */
+declare function assertType<T>(value: T): void;
+
+class UserComponent extends Ember.Component {
+    first = 'Joe';
+    last = 'Smith';
+
+    @alias('first') firstName: string;
+    @and('first', 'last') hasFullName: boolean;
+
+    @computed('last')
+    get lastName() {
+        return this.get('last');
+    }
+
+    @computed('first', 'last')
+    get fullName() {
+        return `${this.get('first')} ${this.get('last')}`;
+    }
+
+    set fullName(value: string) {
+        const [ first, last ] = value.split(' ');
+        this.setProperties({ first, last });
+    }
+}
+
+const component = UserComponent.create();
+assertType<string>(component.get('first'));
+assertType<string>(component.get('last'));
+assertType<string>(component.get('firstName'));
+assertType<string>(component.get('lastName'));
+assertType<boolean>(component.get('hasFullName'));
+assertType<string>(component.get('fullName'));
---
+++ types/ember-decorators/index.d.ts
@@ -0,0 +1,14 @@
+// Type definitions for ember-decorators 1.2
+// Project: https://github.com/ember-decorators/ember-decorators
+// Definitions by: Derek Wickern <dwickern@gmail.com>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.4
+
+declare module 'ember-decorators/object/computed' {
+    export function alias(dependentKey: string): PropertyDecorator;
+    export function and(...keys: string[]): PropertyDecorator;
+}
+
+declare module 'ember-decorators/object' {
+    export default function(...keys: string[]): PropertyDecorator;
+}
