e4f2cc1ef8f3773aab1a74b2e77b8213dad7ace0
Derek Wickern <dwickern@gmail.com>
2017-08-27 12:14:43 -0700

    add typings for ember-cli-qunit

---
+++ types/ember-cli-qunit/index.d.ts
@@ -0,0 +1,6 @@
+// Type definitions for ember-cli-qunit 4.0
+// Project: https://github.com/ember-cli/ember-cli-qunit
+// Definitions by: Derek Wickern <https://github.com/dwickern>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+export function start(): void;
