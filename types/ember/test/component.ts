e8e19a750c4f9af3efbf795aac63b6691b9fe2b0
theroncross <theroncross@gmail.com>
2017-08-28 19:23:58 -0500

    Refactor component tests as classes

--- types/ember/test/component.ts
+++ types/ember/test/component.ts
@@ -1,19 +1,19 @@
 import Ember from 'ember';
 import Component from '@ember/component';
-import Object, { computed } from '@ember/object'
+import Object, { computed } from '@ember/object';
 import hbs from 'htmlbars-inline-precompile';
 import { assertType } from "./lib/assert";
 
 Component.extend({
-    layout: hbs`
+  layout: hbs`
         <div>
           {{yield}}
         </div>
-    `
+    `,
 });
 
 Component.extend({
-    layout: 'my-layout'
+  layout: 'my-layout',
 });
 
 const MyComponent = Component.extend();
@@ -26,92 +26,98 @@ const component1 = Component.extend({
     },
   },
 });
+class Comp1 extends Component {
+  name: string = '';
+  hello(name: string) {
+      this.set('name', name);
+  }
+}
 
-const component2 = Component.extend({
-  tagName: 'em',
-});
+class Comp2 extends Component {
+  tagName: 'em';
+}
 
-const component3 = Component.extend({
-  classNames: ['my-class', 'my-other-class'],
-});
+class Comp3 extends Component {
+  classNames: ['my-class', 'my-other-class'];
+}
 
-const component4 = Component.extend({
-  classNameBindings: ['propertyA', 'propertyB'],
-  propertyA: 'from-a',
-  propertyB: computed(function() {
+class Comp4 extends Component {
+  classNameBindings = ['propertyA', 'propertyB'];
+  propertyA = 'from-a';
+  propertyB = computed(function() {
     if ('someLogic') {
       return 'from-b';
     }
-  }),
-});
+  });
+}
 
-const component5 = Component.extend({
-  classNameBindings: ['hovered'],
-  hovered: true,
-});
+class Comp5 extends Component {
+  classNameBindings = ['hovered'];
+  hovered = true;
+}
 
-const component6 = Component.extend({
-  classNameBindings: ['messages.empty'],
-  messages: Object.create({
+class Comp6 extends Component {
+  classNameBindings = ['messages.empty'];
+  messages = Object.create({
     empty: true,
-  }),
-});
+  });
+}
 
-const component7 = Component.extend({
-  classNameBindings: ['isEnabled:enabled:disabled'],
-  isEnabled: true,
-});
+class Comp7 extends Component {
+  classNameBindings = ['isEnabled:enabled:disabled'];
+  isEnabled = true;
+}
 
-const component8 = Component.extend({
-  classNameBindings: ['isEnabled::disabled'],
-  isEnabled: true,
-});
+class Comp8 extends Component {
+  classNameBindings = ['isEnabled::disabled'];
+  isEnabled = true;
+}
 
-const component9 = Component.extend({
-  tagName: 'a',
-  attributeBindings: ['href'],
-  href: 'http://google.com',
-});
+class Comp9 extends Component {
+  tagName = 'a';
+  attributeBindings = ['href'];
+  href: 'http://google.com';
+}
 
-const component10 = Component.extend({
-  tagName: 'a',
-  attributeBindings: ['url:href'],
-  url: 'http://google.com',
-});
+class Comp10 extends Component {
+  tagName = 'a';
+  attributeBindings = ['url:href'];
+  url = 'http://google.com';
+}
 
-const component11 = Component.extend({
-  tagName: 'use',
-  attributeBindings: ['xlinkHref:xlink:href'],
-  xlinkHref: '#triangle',
-});
+class Comp11 extends Component {
+  tagName = 'use';
+  attributeBindings = ['xlinkHref:xlink:href'];
+  xlinkHref = '#triangle';
+}
 
-const component12 = Component.extend({
-  tagName: 'input',
-  attributeBindings: ['disabled'],
-  disabled: false,
-});
+class Comp12 extends Component {
+  tagName = 'input';
+  attributeBindings = ['disabled'];
+  disabled = false;
+}
 
-const component13 = Component.extend({
-  tagName: 'input',
-  attributeBindings: ['disabled'],
-  disabled: computed(function() {
+class Comp13 extends Component {
+  tagName = 'input';
+  attributeBindings = ['disabled'];
+  disabled = computed(function() {
     if ('someLogic') {
       return true;
     } else {
       return false;
     }
-  }),
-});
+  });
+}
 
-const component14 = Component.extend({
-  tagName: 'form',
-  attributeBindings: ['novalidate'],
-  novalidate: null,
-});
+class Comp14 extends Component {
+  tagName = 'form';
+  attributeBindings = ['novalidate'];
+  novalidate = null;
+}
 
-const component15 = Component.extend({
+class Comp15 extends Component {
   click(event: object) {
     // will be called when an instance's
     // rendered element is clicked
-  },
-});
+  }
+}
