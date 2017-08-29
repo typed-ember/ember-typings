7738779fd05aa9a9d17442fcc01e4a43e82971de
Derek Wickern <dwickern@gmail.com>
2017-08-28 17:40:13 -0700

    fix Object.extend arguments not being type-checked

--- types/ember/index.d.ts
+++ types/ember/index.d.ts
@@ -211,25 +211,35 @@ type CreateWithMixins = <Instance extends M1Base, M1, M1Base, Extensions extends
     args?: Extensions & ThisType<Extensions & Instance & M1>)
     => Extensions & Instance & M1;
 
-type Extend1 = <Instance extends B1, Args extends EmberClassArguments<Instance>, T1 extends Args, B1>(
+type Extend1 = <Instance extends B1,
+    T1 extends EmberClassArguments<Instance>, B1>(
     this: new () => Instance,
     arg1?: MixinOrLiteral<T1, B1> & ThisType<Instance & T1>)
     => EmberClassConstructor<T1 & Instance>;
 
-type Extend2 = <Instance extends B1 & B2, Args extends EmberClassArguments<Instance>, T1 extends Args, B1, T2 extends Args, B2>(
+type Extend2 = <Instance extends B1 & B2,
+    T1 extends EmberClassArguments<Instance>, B1,
+    T2 extends EmberClassArguments<Instance>, B2>(
     this: new () => Instance,
     arg1: MixinOrLiteral<T1, B1> & ThisType<Instance & T1>,
     arg2: MixinOrLiteral<T2, B2> & ThisType<Instance & T1 & T2>)
     => EmberClassConstructor<T1 & T2 & Instance>;
 
-type Extend3 = <Instance extends B1 & B2 & B3, Args extends EmberClassArguments<Instance>, T1 extends Args, B1, T2 extends Args, B2, T3 extends Args, B3>(
+type Extend3 = <Instance extends B1 & B2 & B3,
+    T1 extends EmberClassArguments<Instance>, B1,
+    T2 extends EmberClassArguments<Instance>, B2,
+    T3 extends EmberClassArguments<Instance>, B3>(
     this: new () => Instance,
     arg1: MixinOrLiteral<T1, B1> & ThisType<Instance & T1>,
     arg2: MixinOrLiteral<T2, B2> & ThisType<Instance & T1 & T2>,
     arg3: MixinOrLiteral<T3, B3> & ThisType<Instance & T1 & T2 & T3>)
     => EmberClassConstructor<T1 & T2 & T3 & Instance>;
 
-type Extend4 = <Instance extends B1 & B2 & B3 & B4, Args extends EmberClassArguments<Instance>, T1 extends Args, B1, T2 extends Args, B2, T3 extends Args, B3, T4 extends Args, B4>(
+type Extend4 = <Instance extends B1 & B2 & B3 & B4,
+    T1 extends EmberClassArguments<Instance>, B1,
+    T2 extends EmberClassArguments<Instance>, B2,
+    T3 extends EmberClassArguments<Instance>, B3,
+    T4 extends EmberClassArguments<Instance>, B4>(
     this: new () => Instance,
     arg1: MixinOrLiteral<T1, B1> & ThisType<Instance & T1>,
     arg2: MixinOrLiteral<T2, B2> & ThisType<Instance & T1 & T2>,
@@ -925,6 +935,11 @@ namespace Ember {
         static create(): MapWithDefault;
     }
     class Mixin<T, Base = Ember.Object> {
+        /**
+         * Mixin needs to have *something* on its prototype, otherwise it's treated like an empty interface.
+         */
+        __ember_mixin__: never;
+
         static create<T, Base = Ember.Object>(args?: T & ThisType<T & Base>): Mixin<T, Base>;
     }
     interface MutableArray extends Array, MutableEnumberable {
