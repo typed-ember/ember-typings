b1914f2af4d73314a5b89d4d04d9e3bf2843a1df
Dan Freeman <dfreeman@salsify.com>
2017-08-29 18:16:01 -0400

    Add a type parameter to Enumerable and friends

--- types/ember/test/array.ts
+++ types/ember/test/array.ts
@@ -16,3 +16,17 @@ people.get('lastObject');
 people.isAny('isHappy');
 people.isAny('isHappy', false);
 people.filterBy('isHappy');
+
+assertType<boolean[]>(people.mapBy('isHappy'));
+assertType<any[]>(people.mapBy('name.length'));
+
+assertType<string>(people.get('lastObject').get('name'));
+assertType<boolean>(people.get('firstObject').get('isHappy'));
+
+const letters: Ember.Enumerable<string> = Ember.A(['a', 'b', 'c']);
+const codes: number[] = letters.map((item, index, enumerable) => {
+    assertType<string>(item);
+    assertType<number>(index);
+    assertType<Ember.Enumerable<string>>(enumerable);
+    return item.charCodeAt(0);
+});
