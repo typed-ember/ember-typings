5e9e89f0edbd554a8d3aca2fc26a9e4d1a34e61b
theroncross <theroncross@gmail.com>
2017-09-05 17:14:50 -0500

    Use Component.extends now that it works

--- types/ember/test/component.ts
+++ types/ember/test/component.ts
@@ -26,98 +26,99 @@ const component1 = Component.extend({
     },
   },
 });
-class Comp1 extends Component {
-  name = '';
+
+Component.extend({
+  name: '',
   hello(name: string) {
-      this.set('name', name);
-  }
-}
+    this.set('name', name);
+  },
+});
 
-class Comp2 extends Component {
-  tagName: 'em';
-}
+Component.extend({
+  tagName: 'em',
+});
 
-class Comp3 extends Component {
-  classNames: ['my-class', 'my-other-class'];
-}
+Component.extend({
+  classNames: ['my-class', 'my-other-class'],
+});
 
-class Comp4 extends Component {
-  classNameBindings = ['propertyA', 'propertyB'];
-  propertyA = 'from-a';
-  propertyB = computed(() => {
+Component.extend({
+  classNameBindings: ['propertyA', 'propertyB'],
+  propertyA: 'from-a',
+  propertyB: computed(function() {
     if (!this.get('propertyA')) {
       return 'from-b';
     }
-  });
-}
+  }),
+});
 
-class Comp5 extends Component {
-  classNameBindings = ['hovered'];
-  hovered = true;
-}
+Component.extend({
+  classNameBindings: ['hovered'],
+  hovered: true,
+});
 
-class Comp6 extends Component {
-  classNameBindings = ['messages.empty'];
-  messages = Object.create({
+Component.extend({
+  classNameBindings: ['messages.empty'],
+  messages: Object.create({
     empty: true,
-  });
-}
+  }),
+});
 
-class Comp7 extends Component {
-  classNameBindings = ['isEnabled:enabled:disabled'];
-  isEnabled = true;
-}
+Component.extend({
+  classNameBindings: ['isEnabled:enabled:disabled'],
+  isEnabled: true,
+});
 
-class Comp8 extends Component {
-  classNameBindings = ['isEnabled::disabled'];
-  isEnabled = true;
-}
+Component.extend({
+  classNameBindings: ['isEnabled::disabled'],
+  isEnabled: true,
+});
 
-class Comp9 extends Component {
-  tagName = 'a';
-  attributeBindings = ['href'];
-  href: 'http://google.com';
-}
+Component.extend({
+  tagName: 'a',
+  attributeBindings: ['href'],
+  href: 'http://google.com',
+});
 
-class Comp10 extends Component {
-  tagName = 'a';
-  attributeBindings = ['url:href'];
-  url = 'http://google.com';
-}
+Component.extend({
+  tagName: 'a',
+  attributeBindings: ['url:href'],
+  url: 'http://google.com',
+});
 
-class Comp11 extends Component {
-  tagName = 'use';
-  attributeBindings = ['xlinkHref:xlink:href'];
-  xlinkHref = '#triangle';
-}
+Component.extend({
+  tagName: 'use',
+  attributeBindings: ['xlinkHref:xlink:href'],
+  xlinkHref: '#triangle',
+});
 
-class Comp12 extends Component {
-  tagName = 'input';
-  attributeBindings = ['disabled'];
-  disabled = false;
-}
+Component.extend({
+  tagName: 'input',
+  attributeBindings: ['disabled'],
+  disabled: false,
+});
 
-class Comp13 extends Component {
-  tagName = 'input';
-  attributeBindings = ['disabled'];
-  disabled = computed(() => {
+Component.extend({
+  tagName: 'input',
+  attributeBindings: ['disabled'],
+  disabled: computed(() => {
     if ('someLogic') {
       return true;
     } else {
       return false;
     }
-  });
-}
+  }),
+});
 
-class Comp14 extends Component {
-  tagName = 'form';
-  attributeBindings = ['novalidate'];
-  novalidate = null;
-}
+Component.extend({
+  tagName: 'form',
+  attributeBindings: ['novalidate'],
+  novalidate: null,
+});
 
-class Comp15 extends Component {
+Component.extend({
   click(event: object) {
     // will be called when an instance's
     // rendered element is clicked
-  }
-}
+  },
+});
