f6501e10454bd9a8c07b3261b2ca95215eb801c1
Chris Krycho <chris@chriskrycho.com>
2017-09-13 22:45:01 -0400

    Add stubs for other tests; start on Promise.

--- types/rsvp/index.d.ts
+++ types/rsvp/index.d.ts
@@ -1,5 +1,28 @@
 declare module 'rsvp' {
+    type OnFulfilled<T, F = T> =
+        | ((value: T) => F | PromiseLike<F>)
+        | undefined
+        | null;
+
+    type OnRejected<R> =
+        | ((reason: any) => R | PromiseLike<R>)
+        | undefined
+        | null;
+
     namespace RSVP {
+        class Promise<T> implements PromiseLike<T> {
+            then<TResolved = T, TRejected = never>(
+                onfulfilled?: OnFulfilled<T, TResolved>,
+                onrejected?: OnRejected<TRejected>,
+                label?: string
+            ): PromiseLike<TResolved | TRejected>;
+
+            catch<TRejected = never>(
+                onrejected?: OnRejected<TRejected>,
+                label?: string
+            ): PromiseLike<TRejected>;
+        }
+
         function resolve(): PromiseLike<void>;
         function resolve<T>(t: T): PromiseLike<T>;
     }
--- types/rsvp/rsvp-tests.ts
+++ types/rsvp/rsvp-tests.ts
@@ -1,6 +1,66 @@
 import RSVP from 'rsvp';
+import { assertType } from '../ember/test/lib/assert';
 
-async function testAsync() {
-    let awaitedNothing = await RSVP.resolve();
-    let awaitedValue = await RSVP.resolve('just a value');
+async function testAsyncAwait() {
+    const awaitedNothing = await RSVP.resolve();
+    const awaitedValue = await RSVP.resolve('just a value');
+
+    async function returnsAPromise(): RSVP.Promise<string> {
+        return RSVP.resolve('look, a string');
+    }
+
+    assertType<RSVP.Promise<string>>(returnsAPromise());
+    assertType<string>(await returnsAPromise());
+}
+
+function testPromise() {
+    // TODO: add test
+}
+
+function testAll() {
+    // TODO: add test
+}
+
+function testAllSettled() {
+    // TODO: add test
+}
+
+function testDefer() {
+    // TODO: add test
+}
+
+function testDenodeify() {
+    // TODO: add test
+}
+
+function testFilter() {
+    // TODO: add test
+}
+
+function testHash() {
+    // TODO: add test
+}
+
+function testHashSettled() {
+    // TODO: add test
+}
+
+function testMap() {
+    // TODO: add test
+}
+
+function testRace() {
+    // TODO: add test
+}
+
+function testReject() {
+    // TODO: add test
+}
+
+function testResolve() {
+    // TODO: add test
+}
+
+function testRethrow() {
+    // TODO: add test
 }
