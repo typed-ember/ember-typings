d7c21893c8ea87b18d3f4031a90616d438675643
Derek Wickern <dwickern@gmail.com>
2017-10-01 09:54:47 -0700

    add docs for Ember.Evented

--- types/ember/index.d.ts
+++ types/ember/index.d.ts
@@ -1140,15 +1140,33 @@ export namespace Ember {
         events: EventDispatcherEvents;
     }
     /**
-    This mixin allows for Ember objects to subscribe to and emit events.
-    You can also chain multiple event subscriptions.
-    **/
+     * This mixin allows for Ember objects to subscribe to and emit events.
+     */
     interface Evented {
+        /**
+         * Subscribes to a named event with given function.
+         */
+        on(name: string, target: {}, method: Function): void;
+        /**
+         * Subscribes a function to a named event and then cancels the subscription
+         * after the first time the event is triggered. It is good to use ``one`` when
+         * you only care about the first time an event has taken place.
+         */
+        one(name: string, target: {}, method: Function): void;
+        /**
+         * Triggers a named event for the object. Any additional arguments
+         * will be passed as parameters to the functions that are subscribed to the
+         * event.
+         */
+        trigger(name: string, ...args: any[]): any;
+        /**
+         * Cancels subscription for given name, target, and method.
+         */
+        off(name: string, target: {}, method: Function): void;
+        /**
+         * Checks to see if object has any subscriptions for named event.
+         */
         has(name: string): boolean;
-        off(name: string, target: any, method: Function): Evented;
-        on(name: string, target: any, method: Function): Evented;
-        one(name: string, target: any, method: Function): Evented;
-        trigger(name: string, ...args: any[]): void;
     }
     const Evented: Mixin<Evented>;
     const FROZEN_ERROR: string;
