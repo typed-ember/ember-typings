5a269391d727c744f5e680b4dabeb73a03ef4685
Derek Wickern <dwickern@gmail.com>
2017-10-01 09:19:26 -0700

    add docs for Ember.DefaultResolver

--- types/ember/index.d.ts
+++ types/ember/index.d.ts
@@ -861,14 +861,21 @@ export namespace Ember {
         registerWarnHandler(handler: Function): any;
     };
     /**
-    The DefaultResolver defines the default lookup rules to resolve container lookups before consulting
-    the container for registered items:
-    templates are looked up on Ember.TEMPLATES
-    other names are looked up on the application after converting the name.
-    For example, controller:post looks up App.PostController by default.
-    **/
+     * The DefaultResolver defines the default lookup rules to resolve
+     * container lookups before consulting the container for registered
+     * items:
+     */
     class DefaultResolver extends Resolver {
+        /**
+         * This method is called via the container's resolver method.
+         * It parses the provided `fullName` and then looks up and
+         * returns the appropriate template or class.
+         */
         resolve(fullName: string): {};
+        /**
+         * This will be set to the Application instance when it is
+         * created.
+         */
         namespace: Application;
     }
     /**
