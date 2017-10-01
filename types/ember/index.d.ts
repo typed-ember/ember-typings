6edcc0b66fc89cad840270140e600d3fbc68d8c6
Derek Wickern <dwickern@gmail.com>
2017-10-01 12:08:08 -0700

    move Ember.TextSupport into Ember namespace

--- types/ember/index.d.ts
+++ types/ember/index.d.ts
@@ -178,25 +178,6 @@ interface ClassNamesSupport {
 const ClassNamesSupport: ClassNamesSupport;
 
 /**
-TextSupport is a shared mixin used by both Ember.TextField and Ember.TextArea. TextSupport
-adds a number of methods that allow you to specify a controller action to invoke when a
-certain event is fired on your text field or textarea. The specifed controller action would
-get the current value of the field passed in as the only argument unless the value of the field
-is empty. In that case, the instance of the field itself is passed in as the only argument.
-**/
-interface TextSupport extends TargetActionSupport {
-    cancel(event: Function): void;
-    focusIn(event: Function): void;
-    focusOut(event: Function): void;
-    insertNewLine(event: Function): void;
-    keyPress(event: Function): void;
-    action: string;
-    bubbles: boolean;
-    onEvent: string;
-}
-const TextSupport: Ember.Mixin<TextSupport, Ember.Component>;
-
-/**
 Ember.ActionHandler is available on some familiar classes including Ember.Route,
 Ember.Component, and Ember.Controller. (Internally the mixin is used by Ember.CoreView,
 Ember.ControllerMixin, and Ember.Route and available to the above classes through inheritance.)
@@ -2054,6 +2035,26 @@ export namespace Ember {
          */
         max: string;
     }
+    /**
+     * `TextSupport` is a shared mixin used by both `Ember.TextField` and
+     * `Ember.TextArea`. `TextSupport` adds a number of methods that allow you to
+     * specify a controller action to invoke when a certain event is fired on your
+     * text field or textarea. The specifed controller action would get the current
+     * value of the field passed in as the only argument unless the value of
+     * the field is empty. In that case, the instance of the field itself is passed
+     * in as the only argument.
+     */
+    interface TextSupport extends TargetActionSupport {
+        cancel(event: Function): void;
+        focusIn(event: Function): void;
+        focusOut(event: Function): void;
+        insertNewLine(event: Function): void;
+        keyPress(event: Function): void;
+        action: string;
+        bubbles: boolean;
+        onEvent: string;
+    }
+    const TextSupport: Ember.Mixin<TextSupport, Ember.Component>;
     interface Transition {
         targetName: string;
         urlMethod: string;
