02cf7c70bacfa8dad224425a8dae2bcea1ccaaa7
Arnav Gupta <arnav@codingblocks.com>
2018-02-26 04:27:55 +0530

    generate ember-concurrency initial directory
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

---
+++ types/ember-concurrency/index.d.ts
@@ -0,0 +1,11 @@
+// Type definitions for ember-concurrency 0.8
+// Project: https://github.com/machty/ember-concurrency#readme
+// Definitions by: Arnav Gupta <https://github.com/championswimmer>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+export = ember_concurrency;
+
+declare const ember_concurrency: {
+    name: string;
+};
+
