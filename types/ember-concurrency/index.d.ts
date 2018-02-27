bc4538948492a80598e24e91ecb68c02beb7d27d
Arnav Gupta <arnav@codingblocks.com>
2018-02-27 22:23:48 +0530

    fix dts and lint errors
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- types/ember-concurrency/index.d.ts
+++ types/ember-concurrency/index.d.ts
@@ -2,35 +2,33 @@
 // Project: https://github.com/machty/ember-concurrency#readme
 // Definitions by: Arnav Gupta <https://github.com/championswimmer>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-import * as T from './Task';
+// TypeScript Version: 2.4
+
 import Evented from '@ember/object/evented';
 import RSVP from 'rsvp';
+import { Task, TaskProperty, TaskInstance } from './Task';
 
-declare module 'ember-concurrency' {
-    type Task = typeof T.Task;
-    type TaskInstance = typeof T.TaskInstance;
-    type TaskProperty = typeof T.TaskProperty;
-    const all: typeof RSVP.all;
-    const allSettled: typeof RSVP.allSettled;
-    const hash: typeof RSVP.hash;
-    const race: typeof RSVP.race;
+export { Task, TaskProperty, TaskInstance };
+export const all: typeof RSVP.all;
+export const allSettled: typeof RSVP.allSettled;
+export const hash: typeof RSVP.hash;
+export const race: typeof RSVP.race;
 
-    /**
-     * Returns true if the object passed to it is a
-     * TaskCancelation error. If you call someTask.perform().catch(...)
-     * or otherwise treat a TaskInstance like a promise, you may need
-     * to handle the cancelation of a TaskInstance differently from
-     * other kinds of errors it might throw, and you can use this
-     * convenience function to distinguish cancelation from errors.
-     *
-     * @param {Error} error
-     * @returns {boolean}
-     */
-    function didCancel(error?: Error): boolean;
-    function task(generatorFunction: (...args: any[]) => Generator): T.TaskProperty;
-    function taskGroup(...args: any[]): any;
-    function timeout(ms: number): any;
-    function waitForEvent(object: Evented | RSVP.EventTarget | EventTarget, eventName: string): any;
-    function waitForProperty(object: any, key: string, callback: (...args: any[]) => any): any;
-    function waitForQueue(queueName: string): any;
-}
+/**
+ * Returns true if the object passed to it is a
+ * TaskCancelation error. If you call someTask.perform().catch(...)
+ * or otherwise treat a TaskInstance like a promise, you may need
+ * to handle the cancelation of a TaskInstance differently from
+ * other kinds of errors it might throw, and you can use this
+ * convenience function to distinguish cancelation from errors.
+ *
+ * @param {Error} error
+ * @returns {boolean}
+ */
+export function didCancel(error?: Error): boolean;
+export function task(generatorFunction: (...args: any[]) => Generator): TaskProperty;
+export function taskGroup(...args: any[]): any;
+export function timeout(ms: number): any;
+export function waitForEvent(object: Evented | RSVP.EventTarget | EventTarget, eventName: string): any;
+export function waitForProperty(object: any, key: string, callback: (...args: any[]) => any): any;
+export function waitForQueue(queueName: string): any;
